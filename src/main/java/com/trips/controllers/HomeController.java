4c124cd108fdd9e3698fa3fa6f0bb8b43495cd6e
Lisseth Silva <lisseth@gmail.com>
2026-04-11 12:15:33 -0600

    actualizacion 11-4-26

--- src/main/java/com/trips/controllers/HomeController.java
+++ src/main/java/com/trips/controllers/HomeController.java
@@ -1,9 +1,11 @@
 package com.trips.controllers;
 
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import java.util.Date;
+import java.util.LinkedList;
 
 @Controller
 public class HomeController {
@@ -17,5 +19,19 @@ public class HomeController {
         model.addAttribute("vigente", true);
 
         return "home";
+    }
+    @GetMapping("/listado")
+    public String mostrarListado(Model model) {
+    	
+    	List<String> lista = new LinkedList<String>();
+    	lista.add("En la montana");
+    	lista.add("En la ciudad");
+    	lista.add("En los pueblos");
+    	lista.add("En las playas");
+    	
+    	model.addAttribute("listadoTrips");
+    	
+    	return "listado";
+    	
     }
 }
